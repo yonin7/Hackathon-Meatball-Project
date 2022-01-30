5c3e67af60d6a54a8a79ae30832d080d2bfe3250
yonin7 <yonin789@walla.com>
2022-01-30 10:11:57 +0200

    test route at server

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ app.use(express.json());
 
 app.use('/search', route);
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
