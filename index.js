3c582ff4f6e3d1c2a767366298eb8bfa6740a6f5
Yevgeny-Sh <yevgst7@gmail.com>
2022-01-30 10:29:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const cors = require('cors');
-const route = require('./routes');
+const cors = require("cors");
+const route = require("./routes");
 
-require('./db/mongoose');
+require("./db/mongoose");
 // const publicDirectoryPath = path.join(__dirname, 'client/build');
 
 // app.use(express.static(publicDirectoryPath));
 app.use(cors());
 app.use(express.json());
 
-app.use('/search', route);
+app.use("/search", route);
 
-app.get('/', (req, res) => {
-  res.send('hello');
+app.get("/", (req, res) => {
+  res.send("hello from yevgeny");
 });
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
