db23b884268a9bc33f98e47cb63c814af86b2e08
yarentamir <yarden.tamir@walla.co.il>
2022-01-30 22:38:55 +0200

    homepage design

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Recipe from "./pages/Recipe";
+// import Recipe from "./pages/Recipe";
 
 function AppRoutes() {
   return (
     <>
       <Route path="/" element={<Home />} />
-      <Route path="/Recipe/:id" element={<Recipe />} />
+      {/* <Route path="/Recipe/:id" element={<Recipe />} /> */}
     </>
   );
 }
