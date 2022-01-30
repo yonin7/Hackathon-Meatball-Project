b28e0515cc3898169ad2225b54be191fad08f029
yonin7 <92482173+yonin7@users.noreply.github.com>
2022-01-30 14:54:20 +0200

    schema design

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(publicDirectoryPath));
 app.use(cors());
 app.use(express.json());
 
-app.use('/recpies/:lan/', route);
+app.use('/recpies/:lan', route);
 
 // app.get('/', (req, res) => {
 //   res.send('hellow');
